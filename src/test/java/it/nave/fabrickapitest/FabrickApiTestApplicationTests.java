c225c545d57b29c7c1b9f2eadf56abbe0dab3f66
Claudio Nave <claudionave97@gmail.com>
2023-09-08 22:54:36 +0200

    Apply spotless

--- src/test/java/it/nave/fabrickapitest/FabrickApiTestApplicationTests.java
+++ src/test/java/it/nave/fabrickapitest/FabrickApiTestApplicationTests.java
@@ -1,13 +1,13 @@
 package it.nave.fabrickapitest;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class FabrickApiTestApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {}
 }
