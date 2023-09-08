1e7cec337fb3bf179586d55386e1cef403020a59
Claudio Nave <claudionave97@gmail.com>
2023-09-08 22:30:55 +0200

    First commit

---
+++ src/main/java/it/nave/fabrickapitest/FabrickApiTestApplication.java
@@ -0,0 +1,11 @@
+package it.nave.fabrickapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickApiTestApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FabrickApiTestApplication.class, args);
+  }
+}
---
+++ src/test/java/it/nave/fabrickapitest/FabrickApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.nave.fabrickapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
