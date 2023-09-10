7738f46d246a2cfe67ce75bf3e8357b8c28afbca
Claudio Nave <claudionave97@gmail.com>
2023-09-10 12:46:25 +0200

    Apply license

--- src/main/java/it/nave/fabrickapitest/mapper/TransactionMapper.java
+++ src/main/java/it/nave/fabrickapitest/mapper/TransactionMapper.java
@@ -1,3 +1,21 @@
+/*
+ *Copyright (C) 2023 Claudio Nave
+ *
+ *This file is part of fabrickapitest.
+ *
+ *fabrickapitest is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *fabrickapitest is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with fabrickapitest. If not, see <https://www.gnu.org/licenses/>.
+ */
 package it.nave.fabrickapitest.mapper;
 
 import it.nave.fabrickapitest.entity.TransactionEntity;
